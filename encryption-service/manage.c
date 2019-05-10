48243ba74c68b2bbd7ea94e496fcaa64e68a65d6
Raine Makelainen <raine.makelainen@jolla.com>
2019-05-10 15:24:56 +0300

    [home-encryption] Write temporary encryption key. Contributes to JB#45360

--- encryption-service/manage.c
+++ encryption-service/manage.c
@@ -5,8 +5,12 @@
  */
 
 #include <glib.h>
+#include <glib/gstdio.h>
 #include <gio/gio.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "manage.h"
 
 #define NEMO_UID 100000
@@ -27,6 +31,7 @@ typedef struct {
     gulong signal_handler;
     GList *job_watches;
     guint task;
+    gboolean temporary_encryption_key;
 } manage_data;
 
 static inline void manage_data_free(manage_data *data)
@@ -229,6 +234,14 @@ static void on_signal_from_logind(
 
 static void terminate_user(manage_data *data)
 {
+    if (data->temporary_encryption_key) {
+        int encrypt_home = g_open("/var/lib/sailfish-device-encryption/temporary-encryption-key", O_TRUNC | O_CREAT |  O_WRONLY, S_IRUSR | S_IWUSR);
+        GError *error = NULL;
+        g_close(encrypt_home, &error);
+        if (error)
+            g_error_free(error);
+    }
+
     data->signal_handler = g_signal_connect(
             data->login_manager, "g-signal",
             G_CALLBACK(on_signal_from_logind), data);
@@ -332,13 +345,13 @@ static void got_bus(GObject *proxy, GAsyncResult *res, gpointer user_data)
             NULL, got_systemd_manager, user_data);
 }
 
-gboolean finalize(gpointer user_data)
+void finalize(GMainLoop *main_loop, gboolean temporary_encryption_key)
 {
     manage_data *data = g_new0(manage_data, 1);
-    data->main_loop = (GMainLoop *)user_data;
+    data->main_loop = main_loop;
+    data->temporary_encryption_key = temporary_encryption_key;
     printf("Restarting user session with encrypted home.\n");
     g_bus_get(G_BUS_TYPE_SYSTEM, NULL, got_bus, data);
-    return FALSE;  // Do not start again
 }
 
 // vim: expandtab:ts=4:sw=4
