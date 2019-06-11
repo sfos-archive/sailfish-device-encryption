fb21f2e8a8642391ba4d396600b455959fb1cdab
Tomi Leppänen <tomi.leppanen@jolla.com>
2019-06-11 14:19:10 +0300

    [encryption-service] Copy all content from /home. Fixes JB#46131
    Change preparation scripts to copy all content from /home partition
    (except lost+found) if there is enough space in /tmp. Otherwise just
    /home/nemo is copied and warning is logged.
    
    Remove marker file first instead of last so that preparation is not
    started again on next boot even if /home could not be preserved. In the
    current form encrypting again doesn't work anyway.
    
    Introduces dependency on coreutils because many of those tools are used
    in the script.
    
    Signed-off-by: Tomi Leppänen <tomi.leppanen@jolla.com>

--- encryption-service/preparation/home-encryption-preparation.sh
+++ encryption-service/preparation/home-encryption-preparation.sh
@@ -5,5 +5,34 @@ if [ ! -e /etc/crypttab ]; then
     chmod 600 /etc/crypttab
 fi
 
-usermod --move-home --home /tmp/nemo nemo
+mkdir /tmp/home
+# Calculate 1/3 of /tmp to keep it free even after copying
+EXTRA_SPACE=$(( $(df -k /tmp | grep -Eo '[0-9]+ +[0-9]+ +[0-9]+ +[0-9]+% +/tmp$' | cut -d' ' -f1) / 3 ))
+# globs below miss files that start with two dots
+SPACE_ON_TMP=$(df -k /tmp | grep -Eo '[0-9]+ +[0-9]+% +/tmp$' | cut -d' ' -f1)
+# calculation includes lost+found even though it's not copied to /tmp
+SPACE_NEEDED=$(du -sck /home/.[!.]* /home/* | grep -E $'\ttotal$' | cut -d$'\t' -f1)
+
+if [ $SPACE_ON_TMP -gt $(($SPACE_NEEDED + $EXTRA_SPACE)) ]; then
+    # move all stuff
+    echo "Everything in /home fits to /tmp, copying all"
+    for dir in /home/.[!.]* /home/*; do
+        if [ "$(basename $dir)" != "lost+found" ]; then
+            cp --archive "$dir" "/tmp${dir}"
+        fi
+    done
+else
+    # print warning and move only the necessary stuff
+    >&2 echo "Warning: Not enough space to copy everything from /home to /tmp"
+    if [ $SPACE_ON_TMP -gt $(($(du -sk /home/nemo | cut -d$'\t' -f1) + $EXTRA_SPACE)) ]; then
+        echo "Copying just /home/nemo"
+        cp --archive /home/nemo /tmp/home/nemo
+    else
+        >&2 echo "Warning: Not enough space even for /home/nemo. Creating new."
+        cp --archive /etc/skel /tmp/home/nemo
+        chown --recursive nemo:nemo /tmp/home/nemo
+    fi
+fi
+
+usermod --home /tmp/home/nemo nemo
 systemctl stop home.mount
