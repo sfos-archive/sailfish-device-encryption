c9c8449cef14f2b52b6cd110ba3f80575dd669c8
Tomi Leppänen <tomi.leppanen@jolla.com>
2019-08-06 09:48:33 +0300

    Unify coding style
    80 column lines and variables defined at the beginning of the function.
    
    Signed-off-by: Tomi Leppänen <tomi.leppanen@jolla.com>

--- encryption-service/encrypt.c
+++ encryption-service/encrypt.c
@@ -540,6 +540,7 @@ static void can_format_to_type(
     invocation_data *data = user_data;
     GError *error = NULL;
     const gchar *const *mount_points;
+    UDisksFilesystem *udfs;
 
     if (!udisks_manager_call_can_format_finish(
                 (UDisksManager *)manager, &avail, res, &error)) {
@@ -559,7 +560,9 @@ static void can_format_to_type(
     }
 
     // Unmount if mounted, otherwise encryption fails
-    UDisksFilesystem *udfs = udisks_filesystem_proxy_new_sync(data->connection, G_DBUS_PROXY_FLAGS_NONE, UDISKS_INTERFACE, data->crypto_device_path, NULL, &error);
+    udfs = udisks_filesystem_proxy_new_sync(
+            data->connection, G_DBUS_PROXY_FLAGS_NONE, UDISKS_INTERFACE,
+            data->crypto_device_path, NULL, &error);
     if (!udfs) {
         fprintf(stderr, "%s. Aborting.\n", error->message);
         end_encryption_to_failure(data);
@@ -568,7 +571,8 @@ static void can_format_to_type(
         mount_points = udisks_filesystem_get_mount_points(udfs);
         // If the array is not empty, there is a mount point
         if (mount_points != NULL && *mount_points != NULL) {
-            if (!udisks_filesystem_call_unmount_sync(udfs, g_variant_new("a{sv}", NULL), NULL, &error)) {
+            if (!udisks_filesystem_call_unmount_sync(
+                        udfs, g_variant_new("a{sv}", NULL), NULL, &error)) {
                 fprintf(stderr, "%s. Aborting.\n", error->message);
                 end_encryption_to_failure(data);
                 g_error_free(error);
