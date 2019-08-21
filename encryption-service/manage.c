c0ef9781437d3f0a18a5040230f99762bf1479b9
Tomi Leppänen <tomi.leppanen@jolla.com>
2019-08-21 09:45:32 +0300

    Remove duplicated code
    Cleaning up a bit.
    
    Signed-off-by: Tomi Leppänen <tomi.leppanen@jolla.com>

--- encryption-service/manage.c
+++ encryption-service/manage.c
@@ -176,6 +176,37 @@ static const gchar *get_unit_action(manage_action action)
     return "";
 }
 
+static GVariant *get_unit_arguments(manage_task task)
+{
+    GVariantBuilder builder;
+
+    switch (task.action) {
+        case START_UNIT:
+        case STOP_UNIT:
+            return g_variant_new("(ss)", task.argument, "replace");
+        case RELOAD_UNITS:
+            return NULL;
+        case ENABLE_UNIT:
+            g_variant_builder_init(&builder, G_VARIANT_TYPE("as"));
+            g_variant_builder_add(&builder, "s", task.argument);
+            return g_variant_new("(asbb)", &builder, FALSE, FALSE);
+        case MASK_UNIT:
+            g_variant_builder_init(&builder, G_VARIANT_TYPE("as"));
+            g_variant_builder_add(&builder, "s", task.argument);
+            // runtime: true, masking is removed on reboot
+            return g_variant_new("(asbb)", &builder, TRUE, FALSE);
+        case UNMASK_UNIT:
+            g_variant_builder_init(&builder, G_VARIANT_TYPE("as"));
+            g_variant_builder_add(&builder, "s", task.argument);
+            return g_variant_new("(asb)", &builder, FALSE);
+        case CREATE_MARKER:
+        case REMOVE_MARKER:
+        case END_OF_MANAGE_TASKS:
+            break;
+    }
+    return NULL;
+}
+
 static void handle_next_unit_task(manage_data *data);
 
 static void unit_changed_state(
@@ -237,51 +268,19 @@ static void remove_file(const char *path)
 
 static void handle_next_unit_task(manage_data *data)
 {
-    GVariantBuilder builder;
     manage_task task = data->tasks[data->task];
 
     switch (task.action) {
         case START_UNIT:
         case STOP_UNIT:
-            g_dbus_proxy_call(
-                    data->systemd_manager, get_unit_action(task.action),
-                    g_variant_new("(ss)", task.argument, "replace"),
-                    G_DBUS_CALL_FLAGS_NONE, -1, NULL,
-                    unit_changing_state, data);
-            break;
         case RELOAD_UNITS:
-            g_dbus_proxy_call(
-                    data->systemd_manager, get_unit_action(task.action),
-                    NULL, G_DBUS_CALL_FLAGS_NONE, -1, NULL,
-                    unit_changing_state, data);
-            break;
         case ENABLE_UNIT:
-            g_variant_builder_init(&builder, G_VARIANT_TYPE("as"));
-            g_variant_builder_add(&builder, "s", task.argument);
-            g_dbus_proxy_call(
-                    data->systemd_manager, get_unit_action(task.action),
-                    g_variant_new("(asbb)", &builder, FALSE, FALSE),
-                    G_DBUS_CALL_FLAGS_NONE, -1, NULL,
-                    unit_changing_state, data);
-            break;
         case MASK_UNIT:
-            g_variant_builder_init(&builder, G_VARIANT_TYPE("as"));
-            g_variant_builder_add(&builder, "s", task.argument);
-            g_dbus_proxy_call(
-                    data->systemd_manager, get_unit_action(task.action),
-                    // runtime: true, masking is removed on reboot
-                    g_variant_new("(asbb)", &builder, TRUE, FALSE),
-                    G_DBUS_CALL_FLAGS_NONE, -1, NULL,
-                    unit_changing_state, data);
-            break;
         case UNMASK_UNIT:
-            g_variant_builder_init(&builder, G_VARIANT_TYPE("as"));
-            g_variant_builder_add(&builder, "s", task.argument);
             g_dbus_proxy_call(
                     data->systemd_manager, get_unit_action(task.action),
-                    g_variant_new("(asb)", &builder, TRUE),
-                    G_DBUS_CALL_FLAGS_NONE, -1, NULL,
-                    unit_changing_state, data);
+                    get_unit_arguments(task), G_DBUS_CALL_FLAGS_NONE, -1,
+                    NULL, unit_changing_state, data);
             break;
         case CREATE_MARKER:
             create_file(task.argument);
